647917197f16b1bf67063d98276a188ab033f7d1
gaoleMeng <gaole@umich.edu>
2018-04-03 19:34:26 -0400

    move the test to correct folder

--- jdbc/src/test/java/edu/umich/verdict/jdbc/VerdictJdbc.java
+++ jdbc/src/test/java/edu/umich/verdict/jdbc/VerdictJdbc.java
@@ -1,8 +1,11 @@
+package edu.umich.verdict.jdbc;
+
 import java.sql.*;
 
 
 
 public class VerdictJdbc {
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("edu.umich.verdict.jdbc.Driver");
         String url = "jdbc:verdict:impala://salat1.eecs.umich.edu:21050/instacart100g";
@@ -10,7 +13,7 @@ public class VerdictJdbc {
         Connection conn = DriverManager.getConnection(url);
         System.out.println("connection finished");
         Statement stmt = conn.createStatement();
-        String sql2 = "select count(order_number) from orders where user_id=1";
+        String sql2 = "select count(order_number) from orders where user_id=2";
         ResultSet rs = stmt.executeQuery(sql2);
 
         ResultSetMetaData rsmd = rs.getMetaData();
@@ -18,10 +21,8 @@ public class VerdictJdbc {
         for (int i = 1; i <= rsmd.getColumnCount(); i++) {
             System.out.println(rsmd.getColumnName(i));
         }
-//        System.out.println(rs.getString(2));
 
         while (rs.next()) {
-//            int tmp = rs.getInt(3);
             System.out.println(rs.getInt(1));
             System.out.println(rs.getString(2));
             System.out.println(rs.getInt(2));
